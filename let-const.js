bc346caa7a2003ad4f50f936ad358c8548cf3392
dewanNz <nuruzzaman.dn@gmail.com>
2020-08-12 10:48:08 +0600

    let-const commit

---
+++ let-const.js
@@ -0,0 +1,19 @@
+const hubby = "huby name";
+// console.log(hubby);
+
+const numbers = [12, 7];
+numbers[1] = 88;
+numbers.push(33);
+numbers.push('dewan');
+// console.log(numbers);
+
+const nayok = {name:"sakib khan", phone:4567};
+// console.log(nayok);
+// const তার ভেল্যুকে চেঞ্জ হতে দেয় না
+
+let patientName = "Mr. Patient01";
+    patientName = "Mrs. Patient02"
+console.log(patientName);
+
+// let তার ভেল্যুকে চেঞ্জ হতে দেয় . এটা scope variable. leak proof.
+
